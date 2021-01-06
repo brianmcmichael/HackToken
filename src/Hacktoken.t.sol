9c817c55e9bce3da5b6d334df7a8f92d07f44d7c
Brian McMichael <brian@brianmcmichael.com>
2021-01-06 15:57:25 -0500

    dapp init Hacktoken

---
+++ src/Hacktoken.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.6.7;
+
+import "ds-test/test.sol";
+
+import "./Hacktoken.sol";
+
+contract HacktokenTest is DSTest {
+    Hacktoken hacktoken;
+
+    function setUp() public {
+        hacktoken = new Hacktoken();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
